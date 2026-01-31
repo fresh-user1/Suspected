520ae9b2a5a2db7f315572f653788d7458931e4b
Achmad Bicharul I <110098541+fresh-user1@users.noreply.github.com>
2026-01-31 12:01:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -185,5 +185,7 @@ def execute_trace():
             return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
 
